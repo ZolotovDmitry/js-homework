aca1c936ac9ca39658da671d7108becc2baf4490
dizo <dizo@list.ru>
2019-04-17 00:25:23 +0300

    3-1,3-3.done,3-4

--- js/3lesson.js
+++ js/3lesson.js
@@ -19,26 +19,31 @@ let arr3 = [4, 8, 15, 16, 23, 42];
 let arr4 = [4, 8, "15", 16, 23, 42];
 compareArr(arr3, arr4);
 
+//2.Дано натуральное число N. Вычислите сумму его цифр, использую рекурсию
+//(строки, массивы и циклы использовать запрещено).
+
 //3.Напишите функцию range, принимающую три аргумента, два обязательных: начало и конец диапазона,
 //третий аргумент - необязательный (если он не задан, шаг равен единице) – шаг для построения массива.
 //Функция возвращает массив, который содержит все числа из него, включая начальное и конечное. Например,
 //вызов функции range(1, 10, 2) должен будет вернуть [1, 3, 5, 7, 9].
 
-function rangeArr (start, end, step){
+function rangeArr (start, end, step=1){
     let arr = [start];
-for (let i = 0; arr[i] + step <= end; i++){
+for (let i = 0; (arr[i] + step <= end); i++){
     // console.log(arr[i] + step)
     // console.log(end)
     // console.log(arr[i] + step <= end)
-    arr.push = arr[i] + step;
+    // arr.push = arr[i] + step;
+    arr.push(arr[i] + step);
     }
     console.log(arr);
 }
-//не понимаю, почему цикл не идёт через if получится? или do while 
 let start = 1;
 let end = 10;
 let step = 2;
 rangeArr (start, end, step);
+rangeArr(3, 15, 3);
+rangeArr(3, 15);
 
 
 //4.Напишите функцию, которая в зависимости от переданного в нее целочисленного аргумента count,
